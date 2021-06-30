ae6cfe70b05d1f20e8273dac00c0979843ca87dd
Zack Hersh <zackshersh@gmail.com>
2021-06-30 19:32:03 -0400

    added react router to app.js, created component and page directories

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        
       <div id="canvCont"></div>
+      </Router>
     </div>
   );
 }
