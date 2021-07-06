89892ad24a0be04d2aafbe372a909e9caf005889
Zack Hersh <zackshersh@gmail.com>
2021-07-06 17:36:53 -0400

    created home page

---
+++ client/public/cityEngine/city.js
@@ -0,0 +1,5 @@
+import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/build/three.module.js';
+
+import * as index from './index.js'
+import * as ts from './tools.js';
+import * as cls from './classes.js'
--- client/src/pages/Editor.js
+++ client/src/pages/Editor.js
@@ -6,27 +6,27 @@ import UserTool from '../components/userTool'
 
 const Editor = () => {
 
-    const [mode, setMode] = useState('place-residential')
+    const [mode, setMode] = useState('place-residential');
 
-    const handleClick = (e) => {
-        let value = e.currentTarget.value;
+    const save = () => {
+        if(document.querySelector('#saveText').value.length >= 2);{
+            console.log('long enough')
+            let data = JSON.parse(document.querySelector('#saveText').value);
+        }
+
     }
 
-    const textArea = (e) => {
-        console.log(e.target);
-        let textArea = document.querySelector('#saveText');
-        console.log(textArea)
-    }
 
     return(
         <div>
+            <p id="runModeProxy">editor</p>
             {/* textarea is a hidden textarea that static scripts exports the buildings to so react can use graphQL */}
             <textarea style={{display:'none'}} id='saveText'></textarea>
             <div id="canvCont"></div>
             <div id="userTools">
             </div>
             <div id="userSave">
-
+                <button onClick={save}>Save Plot</button>
             </div>
         </div>
     )
