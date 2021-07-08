8d6f81939cea81c6fb6368b50ec69605297794a2
Gene Phelps <gpphelpsii@gmail.com>
2021-07-07 21:27:57 -0400

    Added the city typedef

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -32,13 +32,14 @@ const typeDefs = gql`
         users: [User]
         user(_id: String, username: String): User
         me: User
+        city: Plot
     }
 
     type Mutation {
         addUser(username: String!, email: String!, password: String!): Auth
         login(email: String!, password: String!): Auth
         saveBuildings(buildingType: String!, building_position_x: Int!, building_position_y: Int!, building_position_z: Int!): User
-        savePlot(plot_position_x: Int, plot_position_z: Int): User
+        savePlot(plot_position_x: Int, plot_position_z: Int): Plot
         removeBuilding(_id: ID): User
     }
 `;
