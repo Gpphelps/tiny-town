d067d338e30b7bc2df8aece2d48c96a50f98148d
Zack Hersh <zackshersh@gmail.com>
2021-07-09 19:13:28 -0400

    ditching one attempt at opening editor from city, react and static stuff not playing nicely

--- client/public/cityEngine/city.js
+++ client/public/cityEngine/city.js
@@ -79,7 +79,7 @@ export function init(){
     buildPlots()
     buildWorld()
     document.querySelector('canvas').addEventListener('mousemove',userHover)
-    // document.querySelector('canvas').addEventListener('dblclick', userDoubleClick)
+    document.querySelector('canvas').addEventListener('dblclick', userDoubleClick)
 
 };
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -10,38 +10,12 @@ const Home = () => {
     //HERE WILL BE ALL PLOTS QUERY 
     //textarea text content will immedietly be set to the data from the query
 
-    const handleButton = (e) => {
-        console.log('HEY')
-        console.log(e.target)
-
-        let id = e.target.getAttribute('id');
-
-        //city engine scripts store the new plot coords in local storage
-        let newX = localStorage.getItem('plotX')
-        let newZ = localStorage.getItem('plotZ')
-        console.log(newX,newZ)
-
-    }
-
-    const handleDivClick = (e) => {
-        console.log('yuh')
-        setPopupX(e.clientX)
-        setPopupY(e.clientY)
-        setPopupDisplay('flex')
-    }
-
-
-    const [ popupDisplay, setPopupDisplay ] = useState('none')
-    const [ popupX, setPopupX ] = useState(0)
-    const [ popupY, setPopupY ] = useState(0)
-
-    console.log(PlotPopUp)
 
     return (
         <div>
             <textarea style={{display:'none'}} id="plotData"></textarea>
             <p style={{display:'none'}} id="runModeProxy">city</p>
-            <PlotPopUp handleButton={handleButton} popupX={popupX} popupY={popupY} display={popupDisplay}/>
+            <PlotPopUp />
             <div onClick={handleDivClick}  id="canvCont"></div>
         </div>
     )
