e40e72c55342721902dc820f710044a2968a409c
Zack Hersh <zackshersh@gmail.com>
2021-07-10 13:48:10 -0400

    getme bug fixed

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,39 +1,31 @@
 import { gql } from '@apollo/client';
 
-// export const GET_ME = gql`
-//     query me {
-//         me {
-//             _id
-//             username
-//             email
-//             plot {
-//                 plot_position_x
-//                 plot_position_z
-//                 buildings {
-//                     building_position_x
-//                     building_position_y
-//                     building_position_z
-//                     building_color_r
-//                     building_color_g
-//                     building_color_b
-//                     type {
-//                         type
-//                     }
-//                 }
-//             }
-//         }
-//     }
-// `;
-
 export const GET_ME = gql`
-     query me {
-         me {
-             _id
-             username
-             email
-         }
-     }
-`; 
+    query me {
+        me {
+            _id
+            username
+            email
+            plot {
+                plot_position_x
+                plot_position_z
+                buildings {
+                    building_position_x
+                }
+            }
+        }
+    }
+`;
+
+// export const GET_ME = gql`
+//      query me {
+//          me {
+//              _id
+//              username
+//              email
+//          }
+//      }
+// `; 
 
 export const GET_CITY = gql`
     query city {
