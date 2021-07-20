6d873b691ed1486688f12cac242b074bcca515c0
lizaferguson <lizaferguson19@gmail.com>
2021-07-20 13:15:55 -0400

    updated css/editor.js to format usertool buttons

--- client/public/cityEngine/editor.js
+++ client/public/cityEngine/editor.js
@@ -86,6 +86,7 @@ function initUi(){
             name: 'Road',
             value: 'place-road',
             tool: 'Place a road',
+            src: '../assets/images/simpleRoadButton.png',
         },
         {
             name: 'Residential',
@@ -121,6 +122,7 @@ function initUi(){
         },
 
     ]
+
     buttonTemplates.forEach(button => {
         let elem = document.createElement('button');
         elem.textContent = button.name;
@@ -129,13 +131,18 @@ function initUi(){
         let toolTip = document.createElement('p');
         toolTip.textContent = button.tool;
 
+        // let buttonImage = document.createElement('image');
+        // document.buttonImage.src = roadButton;
+
         elem.appendChild(toolTip);
+        // elem.appendChild(buttonImage);
 
         if(button.highlight){
             elem.dataset.highlight = button.highlight;
         }
 
         elem.classList.add('toolButton')
+        // buttonImage.classList.add('buttonImage')
         toolTip.classList.add('tooltiptext')
         cont.appendChild(elem)
     })
