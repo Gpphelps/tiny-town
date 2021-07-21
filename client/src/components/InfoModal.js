5c61d8f4b8e80da6bb4dacf3d7838fd95ef4129a
Zack Hersh <zackshersh@gmail.com>
2021-07-21 15:27:34 -0400

    info modal stuff

--- client/src/components/InfoModal.js
+++ client/src/components/InfoModal.js
@@ -1,10 +1,17 @@
 
 import React from 'react';
 
-const InfoModal = ({message,display,setDisplay}) => {
+const InfoModal = ({message,display,setDisplay,top,left,title,errorMode}) => {
 
     const style = {
-        display: display
+        display: display,
+        top: top,
+        left: left,
+
+    }
+
+    if(errorMode){
+        style.backgroundColor = 'rgba(255,0,0,0.3)';
     }
 
     const handleExitButton = () => {
@@ -14,7 +21,7 @@ const InfoModal = ({message,display,setDisplay}) => {
     return (
         <div style={style} className="infoModalCont">
             <div>
-                <h5>INFO</h5>
+                <h5>{title}</h5>
                 <h5 onClick={handleExitButton}>X</h5>
             </div>
             <p>{message}</p>
