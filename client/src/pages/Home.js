c9ec1cf4f2f3b4a968f1ecd4754f7b5590de5c40
Gene Phelps <gpphelpsii@gmail.com>
2021-07-20 21:40:13 -0400

    Changes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,8 @@
 
-import React, {useState, useEffect} from 'react';
-import { useQuery, useMutation } from '@apollo/client';
+import React, { useEffect } from 'react';
+import { useQuery } from '@apollo/client';
 import PlotPopUp from '../components/PlotPopUp';
-import {GET_CITY} from '../utils/queries'
+import { GET_CITY } from '../utils/queries'
 
 
 const Home = () => {
@@ -10,8 +10,13 @@ const Home = () => {
 
     //HERE WILL BE ALL PLOTS QUERY 
     //textarea text content will immedietly be set to the data from the query
+
     const { data } = useQuery(GET_CITY)
     console.log(data)
+    if (data !== undefined) {
+        console.log(data.city[0].plot[0].plotName);
+    }
+    
 
     let string = JSON.stringify(data)
  
@@ -25,6 +30,7 @@ const Home = () => {
             <p style={{display:'none'}} id="runModeProxy">city</p>
             <PlotPopUp />
             <div id="canvCont"></div>
+            {data && <div>{data.city[0].plot[0].plotName}</div>}
         </div>
     )
 }
