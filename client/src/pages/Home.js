bca23eb2de35f7b797068a69927d56ca7d11207c
Zack Hersh <zackshersh@gmail.com>
2021-07-21 11:15:12 -0400

    removed stuff from get_me query from home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,32 +1,17 @@
 
-import React, { useEffect } from 'react';
-import { useQuery } from '@apollo/client';
+import React, {useState, useEffect} from 'react';
+import { useQuery, useMutation } from '@apollo/client';
 import PlotPopUp from '../components/PlotPopUp';
 import {GET_CITY} from '../utils/queries'
-import {GET_ME} from '../utils/queries'
 
 
 const Home = () => {
 
 
-
-    const { loading, error, meData, refetch } = useQuery(GET_ME);
-
-    useEffect(() => {
-        refetch();
-    },[refetch]);
-
-    
-    const userData = meData?.me;
     //HERE WILL BE ALL PLOTS QUERY 
     //textarea text content will immedietly be set to the data from the query
-
     const { data } = useQuery(GET_CITY)
     console.log(data)
-    if (data !== undefined) {
-        console.log(data.city[0].plot[0].plotName);
-    }
-    
 
     let string = JSON.stringify(data)
  
@@ -38,15 +23,8 @@ const Home = () => {
         <div>
             <textarea style={{display:'none'}} id="plotData"></textarea>
             <p style={{display:'none'}} id="runModeProxy">city</p>
-            {
-                userData ?
-                    <PlotPopUp />
-                    :
-                    <div></div>
-            }
-
+            <PlotPopUp />
             <div id="canvCont"></div>
-            {data && <div>{data.city[0].plot[0].plotName}</div>}
         </div>
     )
 }
